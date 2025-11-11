d8a9ac199dc57f30c7ac6e9e15c744bcfe40d4bb
Phong-Hai <phong.hai004@gmail.com>
2025-11-11 10:18:11 +0700

    Chore: add route, demo data, menu.html

--- domino_project/domino/views.py
+++ domino_project/domino/views.py
@@ -1,9 +1,26 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Product, Category
 
 
 def home(request):
     return render(request, "pages/index.html")
+
+
+# load all the product from database using category as separation
+# when clicked on menu
+def menu_menu(request):
+    categories = Category.objects.prefetch_related('products').all()
+    return render(request, 'pages/menu.html', {'categories': categories})
+
+
+def cart_menu(request):
+    return render(request, "pages/cart.html")
+
+
+def contact_menu(request):
+    return render(request, "pages/contact.html")
+
 
 
