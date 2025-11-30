641153dfa0cf5e69dd3dc65fea477a58308f092f
HeangSengtray <HeangSengtray>
2025-11-30 16:14:09 +0700

    [NEW] Create Table/Database

--- domino_project/domino/models.py
+++ domino_project/domino/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+class Invoice(models.Model):
+    products = models.ManyToManyField(Product, through='InvoiceItem', related_name='invoices')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Invoice {self.id}"
+
+
+class InvoiceItem(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def __str__(self):
+        return f"{self.quantity} x {self.product.product_name} for Invoice {self.invoice.id}"
